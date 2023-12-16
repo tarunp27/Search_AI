189388e4aa6460f4f33194901b73f0fafd0abc79
Tarun Pugalendi <104558132+tarunp27@users.noreply.github.com>
2023-12-16 22:42:55 +0530

    Header comp added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Article, Brand, CTA, Feature, Navbar} from './components'
+import {Footer,Header,Blog, Possibility,Features, WhatGPT3} from './containers'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
