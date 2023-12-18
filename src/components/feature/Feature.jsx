19042cf5d5b5c3ada850d03e892cfbbcb3270c4d
Tarun Pugalendi <104558132+tarunp27@users.noreply.github.com>
2023-12-18 10:16:55 +0530

    Features and About Ai added

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './feature.css'
-const Feature = () => {
+
+const Feature = ({title,text}) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container__features'>
+      <div className='gpt3__features-container__features-title'>{title}</div>
+      <div className='gpt3__features-container__features-text'>{text}</div>
+    </div>
   )
 }
 
